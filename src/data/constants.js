f2e41b953c4ac1c11ccf97319dbf3873f08a70ef
ankitagutte <ankitagutte2007@gmail.com>
2024-06-21 13:34:18 +0530

    updated resume

--- src/data/constants.js
+++ src/data/constants.js
@@ -9,7 +9,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/gutteankita",
   resume:
-    "https://drive.google.com/file/d/12Fq5cNFH2YrzVm-57smMdt6vYm_bnTMM/view?usp=drive_link",
+    "https://drive.google.com/file/d/1u2P1gTYArd4RJJ61Iy8ft3zzXKmeLhiv/view",
   linkedin: "https://www.linkedin.com/in/ankita-gutte-160795241/",
 
 };
